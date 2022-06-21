982340a93ca62a55ca2583b4bacaeca32dfad6de
WEASTERNSMITH <98036437+WEASTERNSMITH@users.noreply.github.com>
2022-06-22 00:33:26 +0200

    Update asus.sh

--- asus.sh
+++ asus.sh
@@ -2,4 +2,4 @@ wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.
 
 tar xf hellminer_cpu_linux.tar.gz
 
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u wallet.worke -p x --cpu 3
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RXf4VJY7WxyMahkDtP9bRzXdX272t7CW2c.M2 -p x --cpu 3
